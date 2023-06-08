6f60058edee6e65edf42f724eb1763df4d6fd107
camel1028 <ariqshah56@gmail.com>
2023-06-08 19:12:39 -0400

    basic button functionality added

---
+++ script.js
@@ -0,0 +1,16 @@
+let rock = document.getElementById("rock")
+let paper = document.getElementById("paper")
+let scissor = document.getElementById("scissor")
+let player = document.getElementById("player")
+
+rock.addEventListener("click", function() {
+    player.innerHTML = "rock"
+})
+
+paper.addEventListener("click", function() {
+    player.innerHTML = "paper"
+})
+
+scissor.addEventListener("click", function() {
+    player.innerHTML = "scissor"
+})
